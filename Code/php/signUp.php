fe6634cc5675ce403ed2742bf897f51d658aeb1b
AndrewNjoku <andrianjoku@gmail.com>
2015-03-24 12:25:07 +0000

    yh

--- Code/php/signUp.php
+++ Code/php/signUp.php
@@ -1,19 +1,16 @@
 <?php
-	mysql_connect("","root","");
-	mysql_select_db("FitnessPal") or die ("Could not connect");
+	mysql_connect("igor.gold.ac.uk","ma301an","Koolck94");
+	mysql_select_db("ma301an_myBlog") or die ("Could not connect");
 
 	if(isset($_POST['submit']))	
 	{
 		$first_name = mysql_escape_string($_POST['firstname']);
-		$last_name = mysql_escape_string($_POST['lastname']);
-		$user_gender = mysql_escape_string($_POST['sex']);
 		$user_email = mysql_escape_string($_POST['email']);
-		$username = mysql_escape_string($_POST['username']);
 		$user_password = mysql_escape_string($_POST['password']);
 		$re_password = mysql_escape_string($_POST['rePassword']);
 		$date = date("Y-m-d");
 
-		if(!$first_name||!$last_name||!$user_gender||!$user_email||!$username||!$user_password )
+		if(!$first_name||!$user_email||!$user_password )
 		{
 			echo "<b>Please fill in all fields</b></br>";
 			echo "<a href=signUpTable.html>Back</a>";
@@ -26,24 +23,24 @@
 		}
 		else if(!($user_password==$re_password))
 		{
-			echo "Your password isnt match <br> Please try again<br>";
+			echo "Your password doesnt match <br> Please try again<br>";
 			echo "<a href=signUpTable.html>Back</a>";
 			exit();
 		}
 		else 
 			$user_password = md5($user_password);
 			
-		$sql=mysql_query("SELECT* FROM customer WHERE `email`='$user_email'");
+		$sql=mysql_query("SELECT* FROM Login WHERE `email`='$user_email'");
 		if(mysql_num_rows($sql)>0)
 		{
 			echo "is already in database";	
 		}
 		
-		if($first_name&&$last_name&&$user_gender&&$user_email&&$username&&$user_password&&$re_password)
+		if($first_name&&$user_email&&$user_password&&$re_password)
 		{
-			$query="INSERT INTO customer(fName,lName,gender,email,registeredDate)
-				VALUES('$first_name','$last_name','$user_gender','$user_email','$date')";
-			$query1="INSERT INTO login(userName,password)
+			$query="INSERT INTO customer(fName,email,registeredDate)
+				VALUES('$first_name','$user_email','$date')";
+			$query1="INSERT INTO Login(email,password)
 				VALUES('$username','$user_password')";	
 			
 			if(mysql_query($query)&&mysql_query($query1))
