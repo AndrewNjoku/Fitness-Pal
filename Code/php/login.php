fd5bc221decfbc2940c7964d7942a9ee5200cd1f
Dannykl <dannykl@hotmail.co.uk>
2015-03-25 21:43:14 +0000

    Update login.php

--- Code/php/login.php
+++ Code/php/login.php
@@ -54,3 +54,38 @@ if ($email==$dbusername&&md5($password)==$dbpassword)
 		die("please enter a username and a password");
 
 ?>
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+TRY THIS AND SEE IF IT IS WORKING
+<?php	
+	$db =mysql_connect("igor.gold.ac.uk","ma301an","Koolck94");
+	$found = mysql_select_db("ma301an_FitnessPal1") or die("couldn't connect");
+	
+	if(isset($_POST['submit']))
+	{
+		$username=mysql_escape_string($_POST['uName']);
+		$password=mysql_escape_string($_POST['pass']);
+		if(!$username||!$password)
+		{
+			echo("You must fill all fields!<br>");
+			echo "<a href=logIntable.html>Back</a>";
+			exit();
+		}
+		$sql=mysql_query("SELECT* FROM login WHERE `userName`='$username'
+			AND `password`='$password'");
+		if(mysql_num_rows($sql)>0)
+		{
+			echo("You logged in");
+			echo $query;
+			exit();
+		}
+		else 
+			echo "Incorrect username and password <br>";
+			echo "<a href=logIntable.html>Back</a>";
+	}
+	else 
+	{
+		
+	}
+?>
+
+
