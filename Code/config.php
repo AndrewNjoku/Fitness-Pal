c9160ea8eb9ff9e5bb8a600ba4c512805487faa3
AndrewNjoku <andrianjoku@gmail.com>
2015-03-26 01:35:27 +0000

    test

---
+++ Code/config.php
@@ -0,0 +1,3 @@
+<?php
+$con = mysqli_connect('igor.gold.ac.uk','ma301an','','Koolck94');
+?>
--- Code/process.php
+++ Code/process.php
@@ -1,71 +1,69 @@
-
-
 <?php
+include('config.php');
 
+$type = $_POST['type'];
 
-
-session_start();
-
-
-
-
-if($type == 'new') {
-	
-	$connect = mysqli_connect("igor.gold.ac.uk","ma301an","Koolck94") or die ("couldn't connect!");
-    mysqli_select_db("ma301an_FitnessPal1") or die("couldn't find db");
-	
- &nbsp $startdate = $_POST['startdate'].'+'.$_POST['zone'];
- &nbsp $title = $_POST['title'];
- &nbsp $insert = mysqli_query($con,"INSERT INTO calendar(`title`, `startdate`, `enddate`, `allDay`) VALUES('$title','$startdate','$startdate','false')");
- &nbsp $lastid = mysqli_insert_id($con);
- &nbsp echo json_encode(array('status'=>'success','eventid'=>$lastid));
+if($type == 'new')
+{
+	$startdate = $_POST['startdate'].'+'.$_POST['zone'];
+	$title = $_POST['title'];
+	$insert = mysqli_query($con,"INSERT INTO calendar(`title`, `startdate`, `enddate`, `allDay`) VALUES('$title','$startdate','$startdate','false')");
+	$lastid = mysqli_insert_id($con);
+	echo json_encode(array('status'=>'success','eventid'=>$lastid));
 }
 
-if($type == 'changetitle') {
-	
-	$connect = mysqli_connect("igor.gold.ac.uk","ma301an","Koolck94") or die ("couldn't connect!");
-    mysqli_select_db("ma301an_FitnessPal1") or die("couldn't find db");
-	
-	
-   $eventid = $_POST['eventid'];
-   $title = $_POST['title'];
-   $update = mysqli_query($con,"UPDATE calendar SET title='$title' where id='$eventid'");
-   if($update)
-     echo json_encode(array('status'=>'success'));
-   else
-     echo json_encode(array('status'=>'failed'));
+if($type == 'changetitle')
+{
+	$eventid = $_POST['eventid'];
+	$title = $_POST['title'];
+	$update = mysqli_query($con,"UPDATE calendar SET title='$title' where id='$eventid'");
+	if($update)
+		echo json_encode(array('status'=>'success'));
+	else
+		echo json_encode(array('status'=>'failed'));
 }
 
-if($type == 'resetdate') {
-	
-	
-	$connect = mysqli_connect("igor.gold.ac.uk","ma301an","Koolck94") or die ("couldn't connect!");
-    mysqli_select_db("ma301an_FitnessPal1") or die("couldn't find db");
-
-
-   $title = $_POST['title'];
-   $startdate = $_POST['start'];
-   $enddate = $_POST['end'];
-   $eventid = $_POST['eventid'];
-   $update = mysqli_query($con,"UPDATE calendar SET title='$title', startdate = '$startdate', enddate = '$enddate' where id='$eventid'");
-   if($update)
-     echo json_encode(array('status'=>'success'));
-   else
-     echo json_encode(array('status'=>'failed'));
+if($type == 'resetdate')
+{
+	$title = $_POST['title'];
+	$startdate = $_POST['start'];
+	$enddate = $_POST['end'];
+	$eventid = $_POST['eventid'];
+	$update = mysqli_query($con,"UPDATE calendar SET title='$title', startdate = '$startdate', enddate = '$enddate' where id='$eventid'");
+	if($update)
+		echo json_encode(array('status'=>'success'));
+	else
+		echo json_encode(array('status'=>'failed'));
 }
 
-if($type == 'remove') {
-	
-	$connect = mysqli_connect("igor.gold.ac.uk","ma301an","Koolck94") or die ("couldn't connect!");
-    mysqli_select_db("ma301an_FitnessPal1") or die("couldn't find db");
-	
-   $eventid = $_POST['eventid'];
-   $delete = mysqli_query($con,"DELETE FROM calendar where id='$eventid'");
-   if($delete)
-     echo json_encode(array('status'=>'success'));
-   else
-     echo json_encode(array('status'=>'failed'));
+if($type == 'remove')
+{
+	$eventid = $_POST['eventid'];
+	$delete = mysqli_query($con,"DELETE FROM calendar where id='$eventid'");
+	if($delete)
+		echo json_encode(array('status'=>'success'));
+	else
+		echo json_encode(array('status'=>'failed'));
+}
+
+if($type == 'fetch')
+{
+	$events = array();
+	$query = mysqli_query($con, "SELECT * FROM calendar");
+	while($fetch = mysqli_fetch_array($query,MYSQLI_ASSOC))
+	{
+	$e = array();
+    $e['id'] = $fetch['id'];
+    $e['title'] = $fetch['title'];
+    $e['start'] = $fetch['startdate'];
+    $e['end'] = $fetch['enddate'];
+
+    $allday = ($fetch['allDay'] == "true") ? true : false;
+    $e['allDay'] = $allday;
+
+    array_push($events, $e);
+	}
+	echo json_encode($events);
 }
 
 
-?>
