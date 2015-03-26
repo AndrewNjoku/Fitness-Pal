02bc8e0aed4dd1c349656336ddaba719e5e51f0b
AndrewNjoku <andrianjoku@gmail.com>
2015-03-26 21:43:39 +0000

    changes for timetable

--- Code/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con = mysqli_connect('igor.gold.ac.uk','ma301an','Koolck94','Fitness_Pal1');
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
-?>
-
