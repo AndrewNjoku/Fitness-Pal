c9e99a4f8685f6147a9d7be03311e9687d201588
AndrewNjoku <andrianjoku@gmail.com>
2015-03-26 02:44:12 +0000

    php changes

--- Code/config.php
+++ Code/config.php
@@ -1,3 +1,9 @@
 <?php
-$con = mysqli_connect('igor.gold.ac.uk','ma301an','','Koolck94');
+$con = mysqli_connect('igor.gold.ac.uk','ma301an','Koolck94','Fitness_Pal1');
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
 ?>
+
