3ca4d3363b1ff30be87df6268f50a7025b908612
AndrewNjoku <andrianjoku@gmail.com>
2015-03-27 14:55:12 +0000

    some final changes

--- Code/javascript/menu.js
+++ Code/javascript/menu.js
@@ -17,3 +17,10 @@ $(document).ready(function() {
 });
 
 
+ $(window).scroll(function(){
+    $('#nav_list ').css({
+        'left': $(this).scrollLeft() + 15 //Why this 15, because in the CSS, we have set left 15, so as we scroll, we would want this to remain at 15px left
+    });
+});
+
+
